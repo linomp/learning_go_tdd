e79ba1257983806d91ee24709b35a23837fdf45d
linomp <lm2p@hotmail.es>
2019-11-12 23:21:33 +0100

    refactor with switch

---
+++ ch2_hello_world/hello.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// Domain code
+const spanishCode = "Spanish"
+const frenchCode = "French"
+
+// Hello greets a person or the world!
+func Hello(name string, langCode string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return getPrefix(langCode) + name
+}
+
+// declaring a named return value
+func getPrefix(langCode string) (greetingPrefix string) {
+
+	switch langCode {
+	case spanishCode:
+		greetingPrefix = "Hola, "
+	case frenchCode:
+		greetingPrefix = "Bonjour, "
+	default:
+		greetingPrefix = "Hello, "
+	}
+
+	// will return whatever the named return value is set to
+	// (gets initialized at corresponding zero value)
+	return
+}
+
+// Outside world, side effects
+func main() {
+	fmt.Println(Hello("world", ""))
+}
+
+// go build <file>
+// go run <file>
+
+/*
+TDD:
+- Write a test
+- Make the compiler pass
+- Run the test, see that it fails and check the error message is meaningful (important because it's hard to work when tests fail without clear explanation)
+- Write enough code to make the test pass
+- Refactor
+*/
--- ch2_hello_world/hello_test.go
+++ ch2_hello_world/hello_test.go
@@ -6,7 +6,7 @@ import "testing"
 // tests should be clear specifications of what the code needs to do.
 func TestHello(t *testing.T) {
 
-	assertMessage := func(t *testing.T, got string, want string) {
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
 		t.Helper() // so when it fails it doesn't report this line but where this fn iscalled
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -15,32 +15,45 @@ func TestHello(t *testing.T) {
 
 	// a subtest
 	t.Run("say hello when receiving a name", func(t *testing.T) {
-		got := Hello("Vio")
+		got := Hello("Vio", "")
 		want := "Hello, Vio"
-		assertMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("defaults to hello world upon an empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
-		assertMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greets in Spanish", func(t *testing.T) {
+		got := Hello("Vio", "Spanish")
+		want := "Hola, Vio"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greets in French", func(t *testing.T) {
+		got := Hello("Vio", "French")
+		want := "Bonjour, Vio"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("given unknown language, defaults to English", func(t *testing.T) {
+		got := Hello("Vio", "sdfgsdfg")
+		want := "Hello, Vio"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
 /*
 Run with: go test
-
 Rules:
 	xxx_test.go
 	start with Test...
 	arg: t *testing.T  -> "hook" into the testing framework
-
 fmt placeholders:
 	https://golang.org/pkg/fmt/#hdr-Printing
 	%q -> double quotes
-
 godoc in Go 1.13+
 	go get golang.org/x/tools/cmd/godoc
-
-
 */
